6b7d83a647c707c548c3fcc20860281513e2e95e
mena-hub <mssz.nnia@gmail.com>
2021-01-12 00:21:28 -0300

    Port 5001, avoid conflicts with MongoDB 5000

--- server/app.py
+++ server/app.py
@@ -6,3 +6,8 @@ import json
 @app.route('/')
 def base():
     return Response(response=json.dumps({"Status": "UP"}), status=200, mimetype='application/json')
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5001)
+# Terminal
+# python app.py
